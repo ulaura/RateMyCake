8f2e10ef1a03f45653c35dd6c2f80322576ee8a5
Laura Unaeze <l.unaeze5@gmail.com>
2018-08-06 21:26:37 -0700

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Nothing to see here...yet.
+      </div>
+    );
+  }
+}
+
+export default App;
